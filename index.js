01f92fe48c29a92ec55a6af458e34bba2f6a036e
JOSE LETTS <jletts1@babson.edu>
2018-01-21 17:49:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b
 }
-function devide(a, b) {
+function divide(a, b) {
   return a / b
 }
+
+function inc(n) {
+  return a + b
+}
+
+
+
